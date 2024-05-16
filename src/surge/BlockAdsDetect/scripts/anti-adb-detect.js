490db135803410de9e56a83b688118888489044c
kien8995 <trandongkien.sine@gmail.com>
2024-05-17 00:43:51 +0700

    refactor

--- src/surge/BlockAdsDetect/scripts/anti-adb-detect.js
+++
@@ -1,7 +0,0 @@
-/*!
- substore-tools - version: 1.7.0
- Build fullhash:beda40e993efabcd6e32, chunkhash:9ac90c4b3aa5505548f0 (2024-05-17 24:28:42)
-
- Author: Kien Tran
-*/
-(()=>{(()=>{"use strict";var r={};const o=/(Adblock|blocking|deblocker|pagead)/g;let{body:e}=$response;const s=/(<|%3C)script[\s\S]*?(>|%3E)[\s\S]*?(<|%3C)(\/|%2F)script[\s\S]*?(>|%3E)/g;let t=(e==null?void 0:e.match(s))||[];t=(t==null?void 0:t.filter(c=>c.match(o)))||[];for(const c in t)e=e.replace(t[c],"");$done({body:e})})();})();
--- src/surge/Quizlet/scripts/quizlet.js
+++
@@ -1,7 +0,0 @@
-/*!
- substore-tools - version: 1.7.0
- Build fullhash:beda40e993efabcd6e32, chunkhash:5d28928c92120aa07c8d (2024-05-17 24:28:42)
-
- Author: Kien Tran
-*/
-(()=>{(()=>{"use strict";var g={};t('"_isEligibleForFreeTrial":\\w+@"type":\\d','"_isEligibleForFreeTrial":false@"type":1');function t(){let{body:e}=$response;if(arguments[0].includes("@")){const s=arguments[0].split("@"),n=arguments[1].split("@");for(let r=0;r<s.length;r++){const l=new RegExp(s[r],"g");e=e.replace(l,n[r])}}else{const s=new RegExp(arguments[0],"g");e=e.replace(s,arguments[1])}$done({body:e})}})();})();

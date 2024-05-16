6977df12d0148dbf0768b0fc07087c6be7a38e67
kien8995 <null>
2024-05-16 17:28:46 +0000

    ✅ created local 'src/sub-store/' from remote 'dist/sub-store/'

---
+++ src/sub-store/proxy-front-operator.js
@@ -0,0 +1,7 @@
+/*!
+ substore-tools - version: 1.7.0
+ Build fullhash:beda40e993efabcd6e32, chunkhash:5a17b5a086acc47da24e (2024-05-17 24:28:42)
+
+ Author: Kien Tran
+*/
+(()=>{(()=>{"use strict";var p={3183:(e,t,r)=>{var o;typeof window!="undefined"?o=window:typeof r.g!="undefined"?o=r.g:typeof self!="undefined"?o=self:o={},e.exports=o}},f={};function n(e){var t=f[e];if(t!==void 0)return t.exports;var r=f[e]={exports:{}};return p[e](r,r.exports,n),r.exports}n.n=e=>{var t=e&&e.__esModule?()=>e.default:()=>e;return n.d(t,{a:t}),t},n.d=(e,t)=>{for(var r in t)n.o(t,r)&&!n.o(e,r)&&Object.defineProperty(e,r,{enumerable:!0,get:t[r]})},n.g=function(){if(typeof globalThis=="object")return globalThis;try{return this||new Function("return this")()}catch(e){if(typeof window=="object")return window}}(),n.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t);var b={};(()=>{var e=n(3183),t=n.n(e),r=(u,i,_)=>new Promise((d,l)=>{var v=a=>{try{s(_.next(a))}catch(c){l(c)}},w=a=>{try{s(_.throw(a))}catch(c){l(c)}},s=a=>a.done?d(a.value):Promise.resolve(a.value).then(v,w);s((_=_.apply(u,i)).next())});function o(){return r(this,arguments,function*(u=[],i,_){return u})}t().operator=o})()})();})();

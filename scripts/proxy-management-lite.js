1644afa080d0dceeafaa9aed53b6ba6a937ec35d
kien8995 <null>
2024-05-16 08:12:38 +0000

    ✅ synced local 'scripts/' with remote 'dist/sub-store/tools/'

--- scripts/proxy-management-lite.js
+++ scripts/proxy-management-lite.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.6.0
- Build fullhash:614ae63eb6136f9f99b3, chunkhash:e3da4fd0f28b81eb679f (2024-05-16 15:09:33)
+ Build fullhash:04adabf7baf635c6bcf1, chunkhash:e3da4fd0f28b81eb679f (2024-05-16 15:12:31)
 
  Author: Kien Tran
 */
